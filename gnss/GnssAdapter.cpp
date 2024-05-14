4d697695e9f232bbe4e6ff19c6283b701fdbe2cb
kparmar <quic_kparmar@quicinc.com>
2024-05-14 13:11:41 +0530

    Compute VRP postion for SPE reports if EHUB is disabled
    SPE reports are capable to compute VRP based position
    when in motion and Lever arm parameters are configured
    Changes call VRP based postion calculation when EHUB is
    disabled.
    
    Change-Id: I0102c5888e541b030d0eadef0debbe28e6084c29
    CRs-Fixed: 3816018

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -4254,13 +4254,6 @@ GnssAdapter::computeVRPBasedLla(const UlpLocation& loc, GpsLocationExtended& loc
     double lla[3];
 
     uint16_t locFlags = loc.gpsLocation.flags;
-    uint64_t locExtFlags = locExt.flags;
-
-    // check for SPE fix
-    if (!((locExtFlags & GPS_LOCATION_EXTENDED_HAS_OUTPUT_ENG_TYPE) &&
-          (locExt.locOutputEngType == LOC_OUTPUT_ENGINE_SPE))){
-        return;
-    }
 
     // we can only do translation if we have VRP based lever ARM info
     LeverArmTypeMask leverArmFlags = leverArmConfigInfo.leverArmValidMask;
@@ -4365,6 +4358,10 @@ GnssAdapter::reportPositionEvent(const UlpLocation& ulpLocation,
                     mAdapter.mGnssMbSvIdUsedInPosition = mLocationExtended.gnss_mb_sv_used_ids;
                 }
             }
+
+            // obtain the VRP based latitude/longitude/altitude for SPE fix
+            mAdapter.computeVRPBasedLla(mUlpLocation,
+                    mLocationExtended, mAdapter.mLocConfigInfo.leverArmConfigInfo);
 
             if (GPS_LOCATION_DESIRED_FLAGS ==
                     (mUlpLocation.gpsLocation.flags & (GPS_LOCATION_DESIRED_FLAGS)) &&
@@ -4862,10 +4859,6 @@ GnssAdapter::reportSpeAsEnginePosition(const UlpLocation& ulpLocation,
         engLocationInfo.locationExtended = locationExtended;
         engLocationInfo.sessionStatus = status;
 
-        // obtain the VRP based latitude/longitude/altitude for SPE fix
-        computeVRPBasedLla(engLocationInfo.location,
-                           engLocationInfo.locationExtended,
-                           mLocConfigInfo.leverArmConfigInfo);
         enginePositionReported = reportEnginePositions(1, &engLocationInfo);
     }
     return enginePositionReported;
